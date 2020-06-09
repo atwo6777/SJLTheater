5fa859b6d265ed6d26156f19d50bf101ec672641
atwo6777 <haystackjma@gmail.com>
2020-06-08 22:35:49 -0700

    Updates

---
+++ precache-manifest.fddc260cf0b6a38b7a96d05645c49e7a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee04ea3570531265f71073d7d6b9ba68",
+    "url": "/SJLTheater/index.html"
+  },
+  {
+    "revision": "8fa3a5f74e788649f076",
+    "url": "/SJLTheater/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f81867e0a461438ad9b6",
+    "url": "/SJLTheater/static/js/2.4ef118ec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/SJLTheater/static/js/2.4ef118ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fa3a5f74e788649f076",
+    "url": "/SJLTheater/static/js/main.e8c6141f.chunk.js"
+  },
+  {
+    "revision": "7f90e05919834507c8b9",
+    "url": "/SJLTheater/static/js/runtime-main.29ab861a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/SJLTheater/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.abd397e427dc7054555c7c222aba2961.js"
+  "/SJLTheater/precache-manifest.fddc260cf0b6a38b7a96d05645c49e7a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/SJLTheater/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
