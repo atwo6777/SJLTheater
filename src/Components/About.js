fefbb16f8561afad89c4f9040418c642e6e53774
atwo6777 <haystackjma@gmail.com>
2020-07-09 15:20:40 -0700

    Update About.js

--- src/Components/About.js
+++ src/Components/About.js
@@ -6,25 +6,45 @@ function About() {
     <div className="about">
       <h2>About</h2>
       <p>
-        Leverage agile frameworks to provide a robust synopsis for high level
-        overviews. Iterative approaches to corporate strategy foster
-        collaborative thinking to further the overall value proposition.
-        Organically grow the holistic world view of disruptive innovation via
-        workplace diversity and empowerment.
+        Stephanie Lutz is a lighting designer that has worked actively in San
+        Francisco and New York State. Her previous positions include House
+        Lighting Designer for The California Theater in Pittsburg, CA (operated
+        by VenueTech Management); As well as more recently being employed by The
+        Fisher Center at Bard as the House Lighting Designer for The
+        Spiegeltent. Through the last seven years they have been involved with
+        more than 50 theatrical productions as a lighting designer, and over
+        3,000 events as positions from Technician, Master Electrician, and
+        Lighting Designer. She is currently studying at University of Idaho
+        after being the Resident Lighting Designer for Los Medanos College for
+        four years. Her anticipated graduation is in April 2021 with a Bachelors
+        of Fine Arts within Theater Design, Technology, and Management with
+        emphasis in Theatrical Lighting Design. During her time at Los Medanos
+        College they were awarded the Vectorworks Award at KCACTF 2016, and at
+        her time at University of Idaho they were awarded a Meritorious
+        Achievement Award for their work on Little Women the Musical.
       </p>
       <p>
-        Bring to the table win-win survival strategies to ensure proactive
-        domination. At the end of the day, going forward, a new normal that has
-        evolved from generation X is on the runway heading towards a streamlined
-        cloud solution. User generated content in real-time will have multiple
-        touchpoints for offshoring.
+        Her theatrical philosophy starts within dramaturgical research into the
+        story to find the motifs that lay throughout the story to bring to the
+        table to collaborate with. From there the amount of abstraction within
+        the conceptual development is, hopefully for them, completely
+        collaborative. Utilizing angles first and foremost with the addition of
+        texture and color that is rooted within the story, she hopes to bring
+        the beauty of the internal world of the piece to the stage.
       </p>
       <p>
-        Capitalize on low hanging fruit to identify a ballpark value added
-        activity to beta test. Override the digital divide with additional
-        clickthroughs from DevOps. Nanotechnology immersion along the
-        information highway will close the loop on focusing solely on the bottom
-        line.
+        She has also had the opportunity to work on projection integrated into
+        their design, from both outside collaborators and designing them
+        herself. She is proficient on ETC Eos, Cobalt, High End Hog systems, and
+        able to manipulate GrandMA consoles at a reasonable caliber. Her
+        programming prowise on Eos has seen her busk (or live program) well into
+        the thousands of concerts, dance, theater, and events.
+      </p>
+      <p>
+        She hopes to go into touring productions; however she holds experience
+        within shop duties and maintenance of varying fixtures both static and
+        moving. As well at being adept at organizational aspects of the
+        production management side of the industry.
       </p>
     </div>
   );
